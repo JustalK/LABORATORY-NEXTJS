ef9f4d8b34615302bb39127a8c3d68eb105fa825
justalk <justal.kevin@gmail.com>
2021-12-05 12:14:22 +0100

    [FEATURE] Adding rendering mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,35 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '@src/styles/Home.module.scss'
 
 export default function Home () {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Static App - justalk</title>
+        <title>STATIC PAGE</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Static App - justalk</h1>
+        <h1 className={styles.title}>RENDERING MODES</h1>
+        <div className={styles.subtitle}>Click on one of the link below</div>
+        <span>
+          <Link href="/ssg">
+            <a>SSG</a>
+          </Link>
+          -
+          <Link href="/ssr">
+            <a>SSR</a>
+          </Link>
+          -
+          <Link href="/isr">
+            <a>ISR</a>
+          </Link>
+          -
+          <Link href="/csr">
+            <a>CSR</a>
+          </Link>
+        </span>
       </main>
 
       <footer className={styles.footer}>
